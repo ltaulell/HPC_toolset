31895e2e9407cd8d2351647a89f7ecdd1eca5d7f
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2020-06-23 13:38:16 +0200

    close TODO, 'be explicit', +linter

--- get_ipmitool.py
+++ get_ipmitool.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# PSMN: $Id: get_ipmitool.py 2938 2020-06-22 07:44:14Z ltaulell $
+# PSMN: $Id: get_ipmitool.py 2940 2020-06-23 11:36:55Z ltaulell $
 # SPDX-License-Identifier: CECILL-B OR BSD-2-Clause
 
-""" use ipmitool to get data from BMC interface """
+""" POC: use ipmitool to get data from BMC interface
 
-# import sys
+Lot of hardcoded things
+"""
+
 import argparse
 import execo
 import csv
@@ -16,11 +18,12 @@ from pprint import pprint
 
 
 def get_data(host, user, password, debug=False):
+    """ use execo as a better subprocess substitute """
     host = host + '-mngt'
     user = user
     password = password
 
-    cmd = "ipmitool -e \& -I lanplus -U {} -P {} -H {} sensor".format(user, password, host)
+    cmd = "ipmitool -I lanplus -U {} -P {} -H {} sensor".format(user, password, host)
     process = execo.Process(cmd, shell=False)
     process.run()
     # split process.stdout line by line
@@ -54,11 +57,25 @@ if __name__ == '__main__':
     result = []
     sensors = get_data(args.host[0], args.user[0], args.password[0], debug=debug)
     for ligne in sensors:
-        if ligne.split('|')[0] == 'Temp             ':
-            # attention aux réponses vides ou 'na'
-            result.append(ligne.split('|')[1].strip())
-        if ligne.split('|')[0] == 'Pwr Consumption  ':
-            result.append(ligne.split('|')[1].strip())
+        # we care about first column's value
+        clef = ligne.split('|')[0].strip()
+        valeur = ligne.split('|')[1].strip()
+
+        # adapt matching to what you want
+        # if clef == 'Memory':  # test for 'na'
+        if clef == 'Temp':  # original one
+            # beware of 'na' values
+            if valeur == 'na':
+                result.append(0)
+            else:
+                result.append(valeur)
+
+        if clef == 'Pwr Consumption':
+            if valeur == 'na':
+                result.append(0)
+            else:
+                result.append(valeur)
+
     if debug:
         pprint(result)
 
