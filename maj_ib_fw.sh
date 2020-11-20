218675bf21ea9f7ef3ac54fc37d230d10874f3cf
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2020-11-20 09:11:42 +0100

    add script to industrial-update IB fw, using EQ tips & tricks

---
+++ maj_ib_fw.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# PSMN: $Id: maj_ib_fw.sh 3046 2020-11-20 08:02:51Z ltaulell $
+
+# set's
+set -euo pipefail # strict mode
+set -vx # Trace each command
+
+BASEPATH="/applis/PSMN/Staff/Maintenance/Mellanox"
+PCI=$(lspci | grep -i ella | awk '{ print $1 }')
+FLINT="${BASEPATH}/mstflint-3.7.0/flint/mstflint"
+GUID=$("${FLINT}" -d "${PCI}" query | grep GUIDs: | awk '{ print $2 }')
+
+cd ${BASEPATH} || { echo "cannot cd to ${BASEPATH}"; exit 1; }
+
+# GUID contain '0300' or 'ffff'
+if [[ "${GUID}" =~ "0300" ]]
+then
+    MAC0=$(echo "${GUID}" | sed -e "s/0300//g")
+
+elif [[ "${GUID}" =~ "ffff" ]]
+then
+    MAC0=$(echo "${GUID}" | sed -e "s/ffff//g")
+
+elif [[ "${GUID}" =~ "e0000" ]]
+then
+    MAC0=$(echo "${GUID}" | sed -e "s/e0000/e/g")
+
+else
+    echo "GUID mismatch !"
+    exit 1
+fi
+
+MAC=$(printf '%X' $((0x$MAC0+0x1)))
+
+PSID=$(${FLINT} -d "${PCI}" q | grep PSID: | awk '{ print $2 }')
+
+case "${PSID}" in
+    MT_1090120019 | HP_0280210019 | ORC1090120019 )
+        FWIMG="MT27500/fw-ConnectX3-rel-2_42_5000-MCX354A-FCB_A2-A5-FlexBoot-3.4.752.bin"
+    ;;
+
+    MT_1100120019 )
+        FWIMG="MT27500/fw-ConnectX3-rel-2_42_5000-MCX353A-FCB_A2-A5-FlexBoot-3.4.752.bin"
+    ;;
+
+    ISL1090110018 )
+        echo "W: FDR10 card !"
+        FWIMG="MT27500/fw-ConnectX3-rel-2_42_5000-MCX354A-QCB_Ax-FlexBoot-3.4.752.bin"
+    ;;
+
+    DEL1100001019 )
+        FWIMG="MT27500/fw-ConnectX3-rel-2_42_5000-079DJ3-FlexBoot-3.4.752.bin"
+    ;;
+
+    * )
+        echo "unrecognized PSID!"
+        exit 1
+    ;;
+
+esac
+
+${FLINT} -d "${PCI}" --allow_psid_change --guid "${GUID}" --mac "${MAC}" -i "${FWIMG}" b
