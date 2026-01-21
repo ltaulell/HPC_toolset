acce586c97b03f73e37609bd3fb91e6a858edcf7
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2026-01-21 15:54:12 +0100

    proper last known working version

--- schedulers/get_acct_utilisateur.py
+++ schedulers/get_acct_utilisateur.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# PSMN: $Id: get_acct_utilisateur.py 4175 2023-10-04 15:32:51Z ltaulell $
-# Authors: J.Barilone
-# Copyright J.Barilone 2023
-# Proofreader: L.Taulelle
-# SPDX-License-Identifier: MIT
+# PSMN: $Id: get_acct_utilisateur.py 5001 2026-01-19 11:16:09Z ltaulell $
+# SPDX-License-Identifier: CECILL-B OR BSD-2-Clause
+
+# travail réalisé par Jessica Barilone (2023)
 
 """
-Sur la base de get_acct_group.sh, remplace get_acct_group.sh
-slurmdbd n'arrive pas à fournir les jobs sur l'année, il faut traiter mois par mois
+Sur la base de get_acct_group.sh
+slurmdbd n'arrive pas à fournir les jobs sur l'année (oom-kill), il faut traiter semaine par semaine
 
-for i in $(sacctmgr -nP list account format=account | xargs);
-do sreport -n job sizes printjobcount Start=2022-01-01 End=2023-01-01 Accounts="${i}" | \
+for i in $(sacctmgr -nP list user format=user | xargs);
+do sreport -n job sizes printjobcount Start=2022-01-01 End=2023-01-01 user="${i}" | \
 awk -F" " '{ SUM +=$3+$4+$5+$6+$7} END {print $2" "SUM}' ;
 done
 
@@ -30,14 +29,15 @@ import pprint
 
 ENTETES = ['user', 'jobs', 'hours']
 MASTER_D = {'': {'user': '', 'jobs': 0, 'hours': 0}}
-EXCLUDES = ['bioensl', 'chimieucbl', 'ecl', 'ensl', 'entreprises', 'root', 'ucbl']
+#EXCLUDES = ['bioensl', 'chimieucbl', 'ecl', 'ensl', 'entreprises', 'root', 'ucbl', 'flixhpc', 'pwsin', 'pwsus']
+EXCLUDES = ['root']
 
 
 def get_options():
     """ get CLI arguments """
     parser = argparse.ArgumentParser(description='compile slurm stats, by date, by user, output a csv file')
     parser.add_argument('-d', action='store_true', help='toggle debug ON (default: no)')
-    parser.add_argument('-f', nargs=1, type=str, help='file to write (default: accounting.csv)', default='accounting.csv')
+    parser.add_argument('-f', nargs=1, type=str, help='file to write (default: accounting.csv)', default=['accounting.csv'])
     parser.add_argument('-y', nargs=1, type=int, help='year of stats (in YYYY form)')
     parser.add_argument('-u', nargs=1, type=str, help='user (default: all)', default='all')
 
@@ -47,8 +47,8 @@ def get_options():
 
 
 def get_accounts(debug=False):
-    """ get full list of slurm accounts """
-    """ ACCOUNTS = $(sacctmgr -nP list account format=account | xargs) """
+    """ get full list of slurm users """
+    """ ACCOUNTS = $(sacctmgr -nP list user format=user | xargs) """
 
     CMD = "sacctmgr -nP list user format=user | xargs"
     acc_out = subprocess.check_output(CMD, shell=True, text=True)
@@ -119,32 +119,45 @@ def get_jobs_sum(MASTER_D, YEAR, debug=False):
     """
 
     for key in MASTER_D.keys():
-        j_key = 0
-        for mois in range(1, 13):  # range est non-inclusif, remember?
-            debut = datetime.date(YEAR, mois, 1)
-            # calendar.monthrange(YEAR, mois)[1] -> dernier jour du mois en cours
-            fin = datetime.date(YEAR, mois, calendar.monthrange(YEAR, mois)[1])
+        if MASTER_D[key]['hours'] == 0:
+            # do not waste time when no computed hours
+            MASTER_D[key]['jobs'] = 0
+        else:
+            j_key = 0
+            for mois in range(1, 13):  # range est non-inclusif, remember?
+                # découper mois en 4 morceaux
+                start = (1, 7)
+                middle = (8, 14)
+                third = (15, 21)
+                last = (22, calendar.monthrange(YEAR, mois)[1])
+                for un, deux in start, middle, third, last:
+                    debut = datetime.date(YEAR, mois, un)
+                    fin = datetime.date(YEAR, mois, deux)
+                    CMD = f"sreport -n job sizes printjobcount start={debut} end={fin} user={key}"
+                    if debug:
+                        print(CMD)
+                    try:
+                        j_out = subprocess.check_output(CMD, shell=True, text=True)
+                        # sreport: error: Invalid user id: {key}
+                        if j_out:
+                            j_out = str(j_out).split()
+                            if debug:
+                                print(j_out)
+                        else:
+                            j_out = [0]
 
-            CMD = f"sreport -n job sizes printjobcount start={debut} end={fin} user={key}"
-            if debug:
-                print(CMD)
-            j_out = subprocess.check_output(CMD, shell=True, text=True)
-            if j_out:
-                j_out = str(j_out).split()
-                if debug:
-                    print(j_out)
-            else:
-                j_out = [0]
+                        # j'ai besoin de $2 à $6 inclus, en tant qu'entiers
+                        j_clean = sum([int(elt) for elt in j_out[2:7]])
 
-            # j'ai besoin de $2 à $6 inclus, en tant qu'entiers
-            j_clean = sum([int(elt) for elt in j_out[2:7]])
+                        if debug:
+                            print(j_clean)
 
-            if debug:
-                print(j_clean)
+                        j_key += j_clean
 
-            j_key += j_clean
+                    except subprocess.CalledProcessError:
+                        pass
 
-        MASTER_D[key]['jobs'] = j_key
+            MASTER_D[key]['jobs'] = j_key
 
 
 def write_csv_file(outfile):
@@ -178,7 +191,7 @@ if __name__ == '__main__':
     if args.y:
         YEAR = int(args.y[0])
 
-    if 'all' in args.u:
+    if 'all' == args.u:
         accounts = get_accounts(debug)
     else:
         accounts = args.u
@@ -192,4 +205,4 @@ if __name__ == '__main__':
     if debug:
         pprint.pprint(MASTER_D)
 
-    write_csv_file(args.f)
+    write_csv_file(args.f[0])
